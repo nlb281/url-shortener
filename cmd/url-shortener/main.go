7bbb7925d62bbb708de9beb289b4eb144a6044db
nlb281 <nlb.marian@gmail.com>
2026-01-13 15:46:22 +0300

    Инициализировал logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev = "development"
 )
 
 func main() {
 	config := config.MustLoad()
 
 	fmt.Println(config)
-	
-	// TODO: init logger
 
+	log := initLogger(config.Env)
+
+	log.Info("starting url-shortener", slog.String("env", config.Env))
+	
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run server
+}
+
+func initLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	return log
 }
