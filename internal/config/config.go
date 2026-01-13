c60ce871db9551a68f9e21f4c50314ec20ec23b6
nlb281 <nlb.marian@gmail.com>
2026-01-13 15:15:02 +0300

    Инициализировал config (правка)

---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address" env-default:"localhost:8080"`
+	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.StringVar(&configPath, "c", "", "path to config file (shorthand)")
+}
+
+func MustLoad() *Config {
+	flag.Parse()
+
+	if configPath == "" {
+		log.Fatal("Config path is required. Use --config or -c flag")
+	}
+
+	_, err := os.Stat(configPath)
+
+	if os.IsNotExist(err) {
+    log.Fatalf("Config file does not exist: %s", configPath)
+  }
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Can't read config: %s", err)
+	}
+
+	return &cfg
+}
