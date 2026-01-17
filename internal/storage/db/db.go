554c1e0e4b01b4dbc75af460b87a09589d77ed1e
nlb281 <nlb.marian@gmail.com>
2026-01-17 21:23:13 +0300

    Добавил функции для URL: GetURL, SaveURL, DeleteURL. БД добавлена в .gitignore

--- internal/storage/db/db.go
+++ internal/storage/db/db.go
@@ -2,15 +2,21 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"url-shortener/internal/storage"
 
 	_ "modernc.org/sqlite" // init driver
 )
 
 type Storage struct {
 	db *sql.DB
+}
+
+func (s *Storage) SaveURl(param1 string, param2 string) {
+	panic("unimplemented")
 }
 
 func New(path string) (*Storage, error) {
@@ -46,4 +52,74 @@ func New(path string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES (?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var url string
+
+	err = stmt.QueryRow(alias).Scan(&url)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", storage.ErrURLNotFound
+		}
+		return "", fmt.Errorf("%s: execute statement: %w", op, err)
+	}
+	
+	return url, nil
+}
+
+func (s *Storage) DeleteURL(alias string) (error) {
+	const op = "storage.sqlite.DeleteURL"
+
+	stmt, err := s.db.Prepare("DELETE FROM url WHERE alias = ?")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(alias)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	n, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if n == 0 {
+		return storage.ErrURLNotFound
+	}
+
+	return nil
 }
