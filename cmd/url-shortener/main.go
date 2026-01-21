9195254f0d98ef3ce744483a506237cb487df355
Daniil <nlb.marian@gmail.com>
2026-01-21 16:59:42 +0300

    Добавил аутентификацию для создания и удаления URL

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -44,9 +44,16 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			config.HTTPServer.User: config.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage))
+		r.Delete("/{alias}", delete.New(log, storage))
+	})
+	
 	router.Get("/{alias}", redirect.New(log, storage))
-	router.Delete("/url/{alias}", delete.New(log, storage))
 
 	log.Info("Start server", slog.String("Address", config.Address))
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -17,9 +17,11 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address string `yaml:"address" env-default:"localhost:8080"`
-	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+	Address 		string `yaml:"address" env-default:"localhost:8080"`
+	Timeout 		time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	User 				string `yaml:"user" env-required:"true"`
+	Password 		string `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 var configPath string
 
